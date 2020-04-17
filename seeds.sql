9635745f9ae149ca6930d70f5598b6f25e7b68d7
Jason Strouphauer <jdstroup10@gmail.com>
2020-04-17 13:39:43 -0500

    committing tables

---
+++ seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (firstname, lastname, streetaddress1, city, state)
+VALUES 
+("DAVE", "GUY", "192 someplace ave", "Austin", "TX"),
+("JIM", "DUDE", "4468 wherever lane", "PORTLAND", "OR"),
+("SAM", "MAN", "9090 nowhere place", "New York City", "NY"),
+
+INSERT INTO items (item, description, price, quantity)
+VALUES 
+("Phone Charger", "Phone charger for iPhone 22 gen X+", 60, 2),
+("Lawn Mowing Service", "Lawn mowing sevice", 100, )
+("Chair", "A nice wooden chair", 220, 5)
